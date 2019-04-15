048cbbbdec445d2090c06a5809bca89892cf6df6
Unknown <s1996ch@gmail.com>
2019-04-15 12:25:32 +0430

    Disable yellow box

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// @Flow
+
 import { AppRegistry } from 'react-native';
 import App from './src/index';
 import { name as appName } from './app.json';
+
+// Hide yellow box
+console.disableYellowBox = true;
 
 AppRegistry.registerComponent(appName, () => App);
